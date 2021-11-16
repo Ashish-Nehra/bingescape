3ad17222acf2cabd7511e0b89883d0872644d349
Ashish Nehra <ashishnehra1198@gmail.com>
2021-11-16 13:54:45 +0530

    Added all pages and routes.
    Added basic structure of pages and imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { JumbotronContainer } from './containers/jumbotron';
-import { FooterContainer } from "./containers/footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home, Browse, Signin, Signup } from "./pages";
+
+import * as ROUTES from "./constants/routes";
 
 export default function App() {
-  return(
-    <>
-    <JumbotronContainer />
-    <FooterContainer />
-    </>
-  ); 
+  return (
+    <Router>
+      <Route exact path='/browse'>
+        <Browse/>
+      </Route>
+
+      <Route exact path='/signin'>
+        <Signin/>
+      </Route>
+
+      <Route exact path='/signup'>
+        <Signup/>
+      </Route>
+
+      <Route exact path={ROUTES.HOME}>
+        <Home/> 
+      </Route>
+    </Router>
+  );
 }
